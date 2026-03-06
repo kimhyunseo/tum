283b948e6ba7e2b62b805fc8ba8c172402f62093
kimhyunseo <kimhyunseo@users.noreply.github.com>
2026-03-06 00:37:59 +0000

    deploy: 98b5de6bd2176f1613fe26e0fd66b1568168f513

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "765126643" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "514630404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
